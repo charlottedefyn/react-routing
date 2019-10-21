228af53dee4c88acae18566a830d7d5233491333
malik <malik.h@webdevpro.net>
2019-10-21 12:13:46 +0200

    ajout du composant BrowserRouter et Route

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <NavBar />
+        <section>
+          {/** de la route la plus précise à la moins précise */}
+          <Route path="/admin" component={Dashboard} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/products/:id" component={ProductDetails} />
+          <Route path="/products" component={Products} />
+          {/** home toujours à la fin */}
+          <Route path="/" component={Home} />
+        </section>
       </div>
     );
   }
