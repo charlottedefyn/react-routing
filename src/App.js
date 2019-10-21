3a099828c24525943c82ae236888aba291478592
malik <malik.h@webdevpro.net>
2019-10-21 12:19:20 +0200

    exact et Switch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -15,13 +15,15 @@ class App extends Component {
       <div className="container">
         <NavBar />
         <section>
-          {/** de la route la plus précise à la moins précise */}
-          <Route path="/admin" component={Dashboard} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/products/:id" component={ProductDetails} />
-          <Route path="/products" component={Products} />
-          {/** home toujours à la fin */}
-          <Route path="/" component={Home} />
+          <Switch>
+            {/** de la route la plus précise à la moins précise */}
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route path="/products" component={Products} />
+            {/** home toujours à la fin */}
+            <Route path="/" exact component={Home} />
+          </Switch>
         </section>
       </div>
     );
