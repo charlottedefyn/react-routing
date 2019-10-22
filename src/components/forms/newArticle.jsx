7efbcdda4350a0da75025afa9b00fa4dd8e52aef
malik <malik.h@webdevpro.net>
2019-10-22 15:24:32 +0200

    synchro des champs saisie dans le formulaire avec le state

--- src/components/forms/newArticle.jsx
+++ src/components/forms/newArticle.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 
 class NewArticle extends Component {
-  state = {};
+  state = {
+    data: { titre: "", contenu: "" }
+  };
+
+  change = e => {
+    const cloneData = { ...this.state.data };
+    cloneData[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ data: cloneData });
+  };
+
   render() {
     return (
       <form>
         <div className="form-group">
           <label htmlFor="titre">Titre</label>
-          <input type="text" className="form-control" id="titre" name="titre" />
+          <input
+            type="text"
+            className="form-control"
+            id="titre"
+            name="titre"
+            value={this.state.data.titre}
+            onChange={this.change}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="contenu">Contenu</label>
@@ -16,6 +32,8 @@ class NewArticle extends Component {
             id="contenu"
             rows="10"
             className="form-control"
+            value={this.state.data.contenu}
+            onChange={this.change}
           ></textarea>
         </div>
         <input type="submit" value="créer" className="btn btn-primary" />
